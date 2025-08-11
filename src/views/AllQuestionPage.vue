376bf726899356a5d579af715aabe4cfff5b1f3c
Sergey <sergeypavlov@redcollar.ru>
2025-08-11 22:44:54 +0300

    some updates

--- src/views/AllQuestionPage.vue
+++ src/views/AllQuestionPage.vue
@@ -89,5 +89,39 @@
         </v-list>
       </v-expansion-panel-text>
     </v-expansion-panel>
+
+    <v-expansion-panel title="JavaScript">
+      <v-expansion-panel-text>
+        <v-list>
+          <v-list-subheader>Функции</v-list-subheader>
+          <v-list-item to="/js/specificity">
+            <v-list-item-title>1.1. Что такое функции?</v-list-item-title>
+          </v-list-item>
+          <v-list-item to="/js/specificity">
+            <v-list-item-title>1.2. Какие функции есть в JS?</v-list-item-title>
+          </v-list-item>
+          <v-list-item to="/js/specificity">
+            <v-list-item-title>1.3. Разница между FunctionDeclaration, FunctionExpression, ArrowFunction</v-list-item-title>
+          </v-list-item>
+          <v-list-subheader>Замыкания</v-list-subheader>
+          <v-list-item to="/css/style-isolation">
+            <v-list-item-title>2.1. Что такое замыкание?</v-list-item-title>
+          </v-list-item>
+          <v-list-item to="/css/isolation-methods">
+            <v-list-item-title>2.2. Лексическое окружение (LexicalEnvironment) переменной?</v-list-item-title>
+          </v-list-item>
+          <v-list-item to="/css/scoped-selectors">
+            <v-list-item-title>2.3. В какой момент создаётся лексическое окружение функции (в момент создания или выполнения)?</v-list-item-title>
+          </v-list-item>
+          <v-list-subheader>Поднятие (Hoisting)</v-list-subheader>
+          <v-list-item to="/css/style-isolation">
+            <v-list-item-title>3.1. Какие конструкции всплывают в JS?</v-list-item-title>
+          </v-list-item>
+          <v-list-item to="/css/isolation-methods">
+            <v-list-item-title>3.2. Можем ли мы обращаться к переменной var до её определения в коде, можем ли это сделать для const, var?</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-expansion-panel-text>
+    </v-expansion-panel>
   </v-expansion-panels>
 </template>
