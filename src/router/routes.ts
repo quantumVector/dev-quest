4c4939c60d10c55549fc16a23fb47749b1df35c9
quantumVector <quantum@MacBook-Air-som-tillhor-quantumVector.local>
2025-12-01 12:48:58 +0300

    some updates

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -172,6 +172,7 @@ import WebVitals from '@/views/web/WebVitals.vue'
 import ChromeDevTools from '@/views/web/ChromeDevTools.vue'
 import SSR from '@/views/web/SSR.vue'
 import BFFInfo from '@/views/web/BFFInfo.vue'
+import WebHTTPTestPage from '@/views/tests/WebHTTPTestPage.vue'
 
 export const routes = [
   {
@@ -869,5 +870,9 @@ export const routes = [
   {
     path: '/web/bff',
     component: BFFInfo,
+  },
+  {
+    path: '/web/http-test',
+    component: WebHTTPTestPage,
   }
 ]
---
+++ src/views/tests/WebHTTPTestPage.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import UniversalTestPage from '@/components/UniversalTestPage.vue'
+import { webHttpQuestions } from '@/data/web-http-questions'
+</script>
+
+<template>
+  <UniversalTestPage
+    :questions="webHttpQuestions"
+    title="Тест по Web: HTTP протокол"
+    description="52 вопроса по темам: Основы HTTP, HTTP методы, HTTP заголовки, Статус-коды, HTTPS и безопасность, Перенаправления, Кеширование"
+    :show-topic-breakdown="true"
+  />
+</template>
